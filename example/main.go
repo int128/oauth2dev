3a3be9cd07ac69cc80d4af313367c443ac22157e
Hidetake Iwata <int128@gmail.com>
2022-12-04 19:12:27 +0900

    Use flag

--- example/main.go
+++ example/main.go
@@ -3,7 +3,7 @@
 //
 // To run this example,
 //
-//	CLIENT_ID=your-client-id CLIENT_SECRET=your-client-secret go run ./example
+//	go run ./example -client-id YOUR_ID -client-secret YOUR_SECRET
 //
 // You need to create an OAuth client before running this example.
 // Open the Google Cloud console https://console.cloud.google.com/apis/credentials,
@@ -13,23 +13,24 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
-	"os"
+	"strings"
 
 	"github.com/int128/oauth2dev"
 	"golang.org/x/oauth2"
 )
 
 func main() {
-	cfg := oauth2.Config{
-		ClientID:     os.Getenv("CLIENT_ID"),
-		ClientSecret: os.Getenv("CLIENT_SECRET"),
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  "https://oauth2.googleapis.com/device/code",
-			TokenURL: "https://oauth2.googleapis.com/token",
-		},
-		Scopes: []string{"email"},
-	}
+	var cfg oauth2.Config
+	var scopes string
+	flag.StringVar(&cfg.Endpoint.AuthURL, "auth-url", "https://oauth2.googleapis.com/device/code", "Authorization endpoint")
+	flag.StringVar(&cfg.Endpoint.TokenURL, "token-url", "https://oauth2.googleapis.com/token", "Token endpoint")
+	flag.StringVar(&cfg.ClientID, "client-id", "", "OAuth Client ID")
+	flag.StringVar(&cfg.ClientSecret, "client-secret", "", "OAuth Client Secret (optional)")
+	flag.StringVar(&scopes, "scopes", "email", "Scopes to request, comma separated")
+	flag.Parse()
+	cfg.Scopes = strings.Split(scopes, ",")
 
 	ctx := context.Background()
 	token, err := oauth2dev.GetToken(ctx, cfg, func(ar oauth2dev.AuthorizationResponse) {
